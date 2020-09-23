587867c78a328f9ef61f901d3b1c3e8e3d023482
朱骏杰 <ories.zhu@mochain.vip>
2020-09-23 22:48:02 +0800

    类组件的复杂state

--- src/App.js
+++ src/App.js
@@ -15,25 +15,25 @@ class Son extends React.Component {
   constructor() {
     super();
     this.state = {
-      n: 0
+      n: 0,
+      m: 0
     };
   }
-  add() {
-    // this.state.n += 1 为什么不行, 因为React没有像vue那样做数据劫持，必须用setState,
-    // React理念是数据不可变，所以必须生成新的对象,再传给setState
+  addN() {
     this.setState({ n: this.state.n + 1 });
-    // 这样写可以，但是由于setState是异步的更新UI的过程，牛x的程序员为了避免混淆了新的state和旧的state而产生bug
-    // this.setState(state=>{
-    //   const n = state.n+1
-    //   console.log(n)
-    //   return {n}
-    // })
+    // m 会被覆盖为 undefined 吗？,不会，第一层自动会沿用
+  }
+  addM() {
+    this.setState({ m: this.state.m + 1 });
+    // n 会被覆盖为 undefined 吗？
   }
   render() {
     return (
       <div className="Son">
         儿子 n: {this.state.n}
-        <button onClick={() => this.add()}>+1</button>
+        <button onClick={() => this.addN()}>n+1</button>
+        m: {this.state.m}
+        <button onClick={() => this.addM()}>m+1</button>
         <Grandson />
       </div>
     );
@@ -46,7 +46,6 @@ const Grandson = () => {
     <div className="Grandson">
       孙子 n:{n}
       <button onClick={() => setN(n + 1)}>+1</button>
-    {/*  setN 永远不会改变N*/}
     </div>
   );
 };
