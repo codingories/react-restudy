d95516e01e7e8a19a0c160835874f441ed940573
朱骏杰 <ories.zhu@mochain.vip>
2020-09-30 22:17:34 +0800

    模拟组件销毁了执行

--- src/App.js
+++ src/App.js
@@ -1,54 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import "./styles.css";
 
-export default function App() {
+const App = (props) => {
+  const [childVisible, setChildVisible] = useState(true)
+  const hide = ()=>{
+    setChildVisible(false)
+  }
+  const show = ()=>{
+    setChildVisible(true)
+  }
   return (
-    <div className="App">
-      爸爸
-      <Son />
+    <div>
+      {childVisible ? <button onClick={hide}>hide</button>:<button onClick={show}>show</button>}
+      {childVisible ? <Child /> : null}
     </div>
-  );
+  )
 }
 
-class Son extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      n: 0,
-      m: 0
-    };
-  }
-  addN() {
-    this.setState({ n: this.state.n + 1 });
-    // m 会被覆盖为 undefined 吗？
-  }
-  addM() {
-    this.setState({ m: this.state.m + 1 });
-    // n 会被覆盖为 undefined 吗？
-  }
-  render() {
-    return (
-      <div className="Son">
-        儿子 n: {this.state.n}
-        <button onClick={() => this.addN()}>n+1</button>
-        m: {this.state.m}
-        <button onClick={() => this.addM()}>m+1</button>
-        <Grandson />
-      </div>
-    );
-  }
+
+const Child = (props)=>{
+  useEffect(()=>{
+    return ()=>{
+      console.log('Child 销毁了')
+    }
+  })
+  return (
+    <div>Child</div>
+  )
 }
 
-const Grandson = () => {
-  const [n, setN] = React.useState(0);
-  const [m, setM] = React.useState(0);
-  return (
-    <div className="Grandson">
-      孙子 n:{n}
-      <button onClick={() => setN(n + 1)}>n+1</button>
-      m:{m}
-      <button onClick={() => setM(m + 1)}>m+1</button>
-    </div>
-  );
-};
+
+export default App
