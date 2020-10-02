3d9a6e3dca20efdcfd13b4be8fa78f60146e6a41
朱骏杰 <ories.zhu@mochain.vip>
2020-10-02 21:59:16 +0800

    setN接收函数

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
 function App() {
-  const [user,setUser] = useState({name:'Frank', age: 18})
+  const [n,setN] = useState(0)
+  // 写成函数js引擎不会立即解析，减少多余的计算过程, 用处不大，多算一次影响不大
   const onClick = ()=>{
-    setUser({
-      name: 'Jack'
-    })
+    // setN(n+1);
+    // setN(n+2);
+    // n 不能+3,而是+2
+    setN(i=>i+1);
+    setN(i=>i+2);
+    // 这样写就能+3,传一个操作,set里面优先使用函数,除非下面不能用再用上面的写法
   }
   return (
     <div className="App">
-      <h1>{user.name}</h1>
-      <h2>{user.age}</h2>
-      <button onClick={onClick}>Click</button>
+      <h1>n: {n}</h1>
+      <button onClick={onClick}>+2</button>
     </div>
   );
 }
