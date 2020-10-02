c9763a241d26a24acb9fa71c8fe806686af07d89
朱骏杰 <ories.zhu@mochain.vip>
2020-10-02 11:43:50 +0800

    myUseState第一版

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
+import ReactDOM from "react-dom"
 
 // import "./styles.css";
 
-const App = () => {
-  console.log('App 运行了')
-  const [n, setN] = React.useState(0);
-  console.log(`n: ${n}`)
-  return (
-    <div className="App">
-      <p>{n}</p>
-      <p>
-        <button onClick={() => setN(n + 1)}>+1</button>
-      </p>
-    </div>
-  )
-}
+
+
 
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js'
 
-// let n = 0
-// const App = ()=> React.createElement('div', {className:'red'}, [
-//   n,
-//   React.createElement('button',{onClick:()=>{
-//     n+=1
-//       ReactDOM.render(
-//         App(),
-//         document.getElementById('root')
-//       );
-//
-//     }},'+1')
-// ])
-//
-// ReactDOM.render(
-//   App(),
-//   document.getElementById('root')
-// );
-//
+const rootElement = document.getElementById('root')
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const myUseState = (initialValue) => {
+  console.log("myUseState run")
+  let state = initialValue;
+  const setState = (newValue)=>{
+    state = newValue
+    render()
+  }
+  return [state, setState]
+};
+
+const render = ()=>{
+  ReactDOM.render(<App />, rootElement)
+}
+
+const App = () => {
+  console.log('App 运行了')
+  const [n, setN] = myUseState(0);
+  console.log(`n:${n}`)
+  return (
+    <div className="Apps">
+      <p>{n}</p>
+      <p>
+        <button onClick={() => setN(n + 1)}>+1</button>
+      </p>
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, rootElement)
