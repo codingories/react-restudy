a7e5847fcdb3ed4fcb832929d4df80f56bc1383b
朱骏杰 <ories.zhu@mochain.vip>
2020-10-02 21:37:54 +0800

    开始学习🧍‍♂️react Hooks

--- src/index.js
+++ src/index.js
@@ -1,27 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.css";
 const rootElement = document.getElementById("root");
 
+const themeContext = React.createContext(null);
+// 上下文其实就是一个全局变量,把themeContext理解为一个全局变量
+
 function App() {
-  const nRef = React.useRef(0); // Ref就是一个很简单的对象。 { current:0 }
-  const log = () => setTimeout(() => console.log(`n: ${nRef.current}`), 1000);
-  const update = React.useState(null)[1];
-  // Vue3里面的current是value
+  const [theme, setTheme] = React.useState("red");
   return (
-    <div className="App">
-      <p>{nRef.current} 这里并不能实时更新</p>
-      <p>
-        <button onClick={() => {
-          nRef.current += 1
-          update(nRef.current)
-        }}>
-          +1
-        </button>
-        <button onClick={log}>log</button>
-      </p>
+    <themeContext.Provider value={{ theme, setTheme }}>
+      {/*value的意思是我们对全局变量进行一次赋值,值就是一个对象*/}
+      {/*themeContext.Provider 表示作用域的开始和结束*/}
+      <div className={`App ${theme}`}>
+        <p>{theme}</p>
+        <div>
+          <ChildA />
+        </div>
+        <div>
+          <ChildB />
+        </div>
+      </div>
+    </themeContext.Provider>
+  );
+}
+
+function ChildA() {
+  const { setTheme } = React.useContext(themeContext);
+  return (
+    <div>
+      <button onClick={() => setTheme("red")}>red</button>
     </div>
   );
 }
-// 那我就是要让Ref重新渲染怎么办，React做不到，就是不会重新渲染
-// 用Vue3吧，Vue能做到，所以Vue3很可能会抢走React一部分用户，因为Api更符合人的直觉
+
+function ChildB() {
+  const { setTheme } = React.useContext(themeContext);
+  return (
+    <div>
+      <button onClick={() => setTheme("blue")}>blue</button>
+    </div>
+  );
+}
+
 ReactDOM.render(<App />, rootElement);
