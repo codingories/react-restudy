2662a9cb46d783d7c0ad2ed31f987342a2adab13
朱骏杰 <ories.zhu@mochain.vip>
2020-10-02 22:25:35 +0800

    reducer 案例3

--- src/index.js
+++ src/index.js
@@ -1,79 +1,139 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useContext, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-const initFormData = {
-  name: "",
-  age: 18,
-  nationality: "汉族"
+const store = {
+  user: null,
+  books: null,
+  movies: null
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case "patch": // 更新
-      return { ...state, ...action.formData }; // 把两个对象合并到第三个对象里
-    case "reset": // 重置
-      return initFormData; // 返回最开始的对象就是重置
+    case "setUser":
+      return { ...state, user: action.user };
+    case "setBooks":
+      return { ...state, books: action.books };
+    case "setMovies":
+      return { ...state, movies: action.movies };
     default:
       throw new Error();
   }
 }
 
+const Context = React.createContext(null);
+
 function App() {
-  const [formData, dispatch] = useReducer(reducer, initFormData);
-  // const patch = (key, value)=>{
-  //   dispatch({ type: "patch", formData: { [key]: value } })
-  // }
-  const onSubmit = () => {};
-  const onReset = () => {
-    dispatch({ type: "reset" });
-  };
+  const [state, dispatch] = useReducer(reducer, store);
+
+  const api = { state, dispatch };
   return (
-    <form onSubmit={onSubmit} onReset={onReset}>
-      <div>
-        <label>
-          姓名
-          <input
-            value={formData.name}
-            onChange={e =>
-              dispatch({ type: "patch", formData: { name: e.target.value } })
-            }
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          年龄
-          <input
-            value={formData.age}
-            onChange={e =>
-              dispatch({ type: "patch", formData: { age: e.target.value } })
-            }
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          民族
-          <input
-            value={formData.nationality}
-            onChange={e =>
-              dispatch({
-                type: "patch",
-                formData: { nationality: e.target.value }
-              })
-            }
-          />
-        </label>
-      </div>
-      <div>
-        <button type="submit">提交</button>
-        <button type="reset">重置</button>
-      </div>
+    <Context.Provider value={api}>
+      <User />
       <hr />
-      {JSON.stringify(formData)}
-    </form>
+      <Books />
+      <Movies />
+    </Context.Provider>
+  );
+}
+
+function User() {
+  const { state, dispatch } = useContext(Context);
+  useEffect(() => {
+    ajax("/user").then(user => {
+      console.log("user");
+      console.log(user);
+      dispatch({ type: "setUser", user: user });
+      console.log(state);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>个人信息</h1>
+      {/* <div>name: {state.user}</div> */}
+      <div>name: {state.user ? state.user.name : ""}</div>
+    </div>
+  );
+}
+
+function Books() {
+  const { state, dispatch } = useContext(Context);
+  useEffect(() => {
+    ajax("/books").then(books => {
+      dispatch({ type: "setBooks", books: books });
+    });
+  }, []);
+  return (
+    <div>
+      <h1>我的书籍</h1>
+      <ol>
+        {state.books
+          ? state.books.map(book => <li key={book.id}>{book.name}</li>)
+          : "加载中"}
+      </ol>
+    </div>
+  );
+}
+
+function Movies() {
+  const { state, dispatch } = useContext(Context);
+  useEffect(() => {
+    ajax("/movies").then(movies => {
+      dispatch({ type: "setMovies", movies: movies });
+    });
+  }, []);
+  return (
+    <div>
+      <h1>我的电影</h1>
+      <ol>
+        {state.movies
+          ? state.movies.map(movie => <li key={movie.id}>{movie.name}</li>)
+          : "加载中"}
+      </ol>
+    </div>
   );
 }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
+
+// 帮助函数
+
+// 假 ajax
+// 两秒钟后，根据 path 返回一个对象，必定成功不会失败
+function ajax(path) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (path === "/user") {
+        resolve({
+          id: 1,
+          name: "Frank"
+        });
+      } else if (path === "/books") {
+        resolve([
+          {
+            id: 1,
+            name: "JavaScript 高级程序设计"
+          },
+          {
+            id: 2,
+            name: "JavaScript 精粹"
+          }
+        ]);
+      } else if (path === "/movies") {
+        resolve([
+          {
+            id: 1,
+            name: "爱在黎明破晓前"
+          },
+          {
+            id: 2,
+            name: "恋恋笔记本"
+          }
+        ]);
+      }
+    }, 2000);
+  });
+}
+
+// 帮助函数
