06600fe93ca9bb42908feee87bf91837fb2811a3
朱骏杰 <ories.zhu@mochain.vip>
2020-10-03 12:17:23 +0800

    useEffect换成useLayOutEffect的例子

--- src/index.js
+++ src/index.js
@@ -1,44 +1,20 @@
-import React, { createContext, useState, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-
 import "./styles.css";
 
-const C = createContext(null);
-// 一.使用 C = createContext(initial)创建上下文
-function App() {
-  console.log("App 执行了");
-  const [n, setN] = useState(0);
-  return (
-    <C.Provider value={{ n, setN }}>
-      {/*二.<C.provider>圈定作用域, 传递一个Value , Value一般会给一个读接口和一个写接口*/}
-      <div className="App">
-        <Baba />
-      </div>
-    </C.Provider>
-  );
-}
+const BlinkyRender = () => {
+  const [value, setValue] = useState(0);
 
-function Baba() {
-  const { n, setN } = useContext(C);
-  return (
-    <div>
-      我是爸爸 n: {n} <Child />
-    </div>
-  );
-}
+  useEffect(() => {
+    document.querySelector('#x').innerText = `value: 1000`
+  }, [value]);
 
-function Child() {
-  const { n, setN } = useContext(C);
-  const onClick = () => {
-    setN(i => i + 1);
-  };
   return (
-    <div>
-      我是儿子 我得到的 n: {n}
-      <button onClick={onClick}>+1</button>
-    </div>
+    <div id="x" onClick={() => setValue(0)}>value: {value}</div>
   );
-}
+};
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <BlinkyRender />,
+  document.querySelector("#root")
+);
