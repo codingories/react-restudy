b988000411673f4913669250a03e6a7deb2778b9
朱骏杰 <ories.zhu@mochain.vip>
2020-10-03 12:21:45 +0800

    react memo例子

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+
 import "./styles.css";
 
-const BlinkyRender = () => {
-  const [value, setValue] = useState(0);
-
-  useEffect(() => {
-    document.querySelector('#x').innerText = `value: 1000`
-  }, [value]);
+function App() {
+  const [n, setN] = React.useState(0);
+  const [m, setM] = React.useState(0);
+  const onClick = () => {
+    setN(n + 1);
+  };
 
   return (
-    <div id="x" onClick={() => setValue(0)}>value: {value}</div>
+    <div className="App">
+      <div>
+        <button onClick={onClick}>update n {n}</button>
+      </div>
+      <Child2 data={m}/>
+      {/* <Child2 data={m}/> */}
+    {/*用memo封装一下，写成这样子就不会再词刷新,memo使得一个组件只有在其props变化的时候才会执行*/}
+    </div>
   );
-};
+}
 
-ReactDOM.render(
-  <BlinkyRender />,
-  document.querySelector("#root")
-);
+function Child(props) {
+  console.log("child 执行了");
+  console.log('假设这里有大量代码')
+  return <div>child: {props.data}</div>;
+}
+
+const Child2 = React.memo(Child);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
