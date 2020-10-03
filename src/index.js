eafb86c46d31b3f79d6fb73f92469196e5851fce
朱骏杰 <ories.zhu@mochain.vip>
2020-10-03 16:20:08 +0800

    useRef案例

--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
-import React from "react";
+import React, {useEffect, useMemo, useRef} from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
 function App() {
+  const count = useRef(0);
   const [n, setN] = React.useState(0);
-  const [m, setM] = React.useState(0);
-  const onClick = () => {
-    setN(n + 1);
-  };
-
+  const onClick = ()=>{
+    setN(n + 9);
+  }
+  useEffect(()=>{
+    count.current+=1
+    console.log(count.current)
+    // 通过useRef记录渲染的次数
+  })
   return (
     <div className="App">
       <div>
         <button onClick={onClick}>update n {n}</button>
       </div>
-      <Child2 data={m}/>
-      {/* <Child2 data={m}/> */}
-    {/*用memo封装一下，写成这样子就不会再词刷新,memo使得一个组件只有在其props变化的时候才会执行*/}
     </div>
   );
 }
 
-function Child(props) {
-  console.log("child 执行了");
-  console.log('假设这里有大量代码')
-  return <div>child: {props.data}</div>;
-}
-
-const Child2 = React.memo(Child);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
