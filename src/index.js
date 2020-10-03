5f8a15890f5c30f32cf619c92a3476f9784e83ed
朱骏杰 <ories.zhu@mochain.vip>
2020-10-03 16:38:08 +0800

    useRef案例

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
-import React, {useEffect, useMemo, useRef} from "react";
+import React, { useRef } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
 function App() {
-  const count = useRef(0);
-  const [n, setN] = React.useState(0);
-  const onClick = ()=>{
-    setN(n + 9);
-  }
-  useEffect(()=>{
-    count.current+=1
-    console.log(count.current)
-    // 通过useRef记录渲染的次数
-  })
+  const buttonRef = useRef(null);
   return (
     <div className="App">
-      <div>
-        <button onClick={onClick}>update n {n}</button>
-      </div>
+      <Button3 ref={buttonRef}>按钮</Button3>
     </div>
   );
 }
 
+const Button2 = (props, ref) => {
+  console.log(props);
+  console.log('ref', ref)
+  return <button className="red" ref={ref} {...props} />;
+};
+// 总结: 如果一个函数组件想要接收别人传来的参数，自己必须用forwardRef包起来
+const Button3 = React.forwardRef(Button2)
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
